b3af490ac6d7d7c847778273330573e6a720a5e3
EmperorKnight <jlpicado@uamv.edu.ni>
2025-04-07 12:50:48 -0600

    Agregado

---
+++ operador_relacionales.py
@@ -0,0 +1,14 @@
+num1=25
+num2=25
+
+print("1) ",num1 > num2)
+print("2) ",num1 < num2)
+print("3) ",num1 == num2)
+print("4) ",num1 != num2)
+print(num1 >= num2)
+print(num1 <= num2)
+print(not (num1) == num2)
+print(not (num1))
+print(not (num1) > num2)
+print(not (num1) < num2)
+print(not (num1) != num2)
