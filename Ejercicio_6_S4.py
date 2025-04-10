1ab701113152aa3cd8a90bfbeb3132df3d07db2f
EmperorKnight <jlpicado@uamv.edu.ni>
2025-04-10 07:32:05 -0600

    Mejorado el ejercicio 6

--- Ejercicio_6_S4.py
+++ Ejercicio_6_S4.py
@@ -1,9 +1,15 @@
 # Crea un algoritmo que dado el año de nacimiento de una persona y el año actual,
 # calcule su edad actual y su edad en 10 años.
 
+from datetime import date
+from datetime import datetime
+
 nacimiento = int(input(f"Introduzca su año de nacimiento \n-> "))
 
-edad_actual = 2025 - nacimiento
+now = datetime.now()
+format = now.strftime("%Y")
+edad_actual = int(format)
+edad_actual -= nacimiento
 edad_futura = edad_actual + 10
 
 print(f"Su edad actual es de {edad_actual}. \nSu edad dentro de 10 años es {edad_futura:.0f}.")
